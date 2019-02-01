588162cff570c01429cb9e31d79af98daa1e2ae4
hquick <h.quick@dynms.de>
2019-02-01 13:47:28 +0100

    Some changes to health specifiy tests

--- pregnancy_calendar.rb
+++ pregnancy_calendar.rb
@@ -58,7 +58,7 @@ get '/:date.ics' do
   cal.add_event(get_event(start_date, 35, 40, "Antrag auf Elterngeld vorbereiten"))
   cal.add_event(get_event(start_date, 35, 40, "Mutterschutz"))
 
-  cal.add_event(get_event(start_date,  5,  5, "Schwangerschaftstest nun aussagekräftig"))
+  cal.add_event(get_event(start_date,  5,  6, "Schwangerschaftstest nun aussagekräftig"))
   cal.add_event(get_event(start_date,  5,  7, "Erstuntersuchung beim Frauenarzt"))
   cal.add_event(get_event(start_date,  9, 12, "1. Ultraschalluntersuchung"))
   cal.add_event(get_event(start_date, 10, 13, "Impfschutz prüfen lassen"))
@@ -68,7 +68,7 @@ get '/:date.ics' do
   cal.add_event(get_event(start_date, 24, 28, "Screening auf Schwangerschaftsdiabetes"))
   cal.add_event(get_event(start_date, 29, 33, "3. Ultraschalluntersuchung"))
   cal.add_event(get_event(start_date, 30, 36, "Geburtsvorbereitungskurs"))
-  cal.add_event(get_event(start_date, 32, 32, "Blutuntersuchungen (Hepatitis B)"))
+  cal.add_event(get_event(start_date, 32, 33, "Blutuntersuchungen (Hepatitis B)"))
   cal.add_event(get_event(start_date, 36, 36, "Abstrich auf B-Streptokokken"))
   
   content_type "text/calendar"
